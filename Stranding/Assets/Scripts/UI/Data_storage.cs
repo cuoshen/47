12452f901a5894bc2dd3e488c75f96ce5c11df67
linzexin <linzexinmasterchief@gmail.com>
2020-10-03 14:13:55 +0800

    ui first draft finished

---
+++ Stranding/Assets/Scripts/UI/Data_storage.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data_storage : MonoBehaviour
+{
+    ///<summary>
+    /// used to store user data
+    /// </summary>
+
+    private int health;
+    private int loop;
+    private int step;
+
+    public Data_storage()
+    {
+        health = 100;
+        loop = 0;
+        step = 0;
+    }
+
+    // getters
+    public int Get_health()
+    {
+        return this.health;
+    }
+
+    public int Get_loop()
+    {
+        return this.loop;
+    }
+
+    public int Get_step()
+    {
+        return this.step;
+    }
+
+    // settters
+    public void Set_health(int _health)
+    {
+        if (_health >= 0 && _health <= 100)
+        {
+            this.health = _health;
+        }
+    }
+
+    public void Set_loop(int _loop)
+    {
+        if (_loop >= 0)
+        {
+            this.loop = _loop;
+        }
+    }
+
+    public void Set_step(int _step)
+    {
+        if (_step >= 0)
+        {
+            this.step = _step;
+        }
+    }
+}
---
+++ Stranding/Assets/Scripts/UI/Health_bar_control.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_bar_control : MonoBehaviour
+{
+    /// <summary>
+    /// used to control progress info ui
+    /// </summary>
+    
+    // player data storage game object
+    public GameObject source;
+    // local data storage script object
+    private Data_storage data_storage;
+
+    public GameObject health_image;
+
+    private int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // initialize data storage
+        data_storage = source.GetComponent<Data_storage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // pull data from data storage
+        health = data_storage.Get_health();
+
+        // update health bar image
+        health_image.GetComponent<RectTransform>().sizeDelta = new Vector2(health, 20);
+    }
+}
