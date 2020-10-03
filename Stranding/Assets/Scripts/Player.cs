f01e1808ec715249cc6a670b002f0834f96b3b7b
Xiao <chairmantoad@gmail.com>
2020-10-03 14:44:26 +0800

    Update data binding

--- Stranding/Assets/Scripts/Player.cs
+++ Stranding/Assets/Scripts/Player.cs
@@ -8,6 +8,25 @@ namespace Stranding
     {
         public int MaxSpeed { get; set; } = 1;
         public int MapPosition { get; set; } = 0;
+        private int _health;
+        public int Health
+        {
+            get
+            {
+                return _health;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    _health = value;
+                }
+                else
+                {
+                    Debug.LogError("Health must be a value between 0 and 100");
+                }
+            }
+        }
 
         public readonly string Name = "Meme man";
     }
