aa122e3df4ea77beb7c2931c8e0fa2b6be91313a
Xiao <chairmantoad@gmail.com>
2020-10-03 16:18:30 +0800

    Prototyping pop up windows

--- Stranding/Assets/Scripts/UI/Pop_up_system.cs
+++ Stranding/Assets/Scripts/UI/Pop_up_system.cs
@@ -2,29 +2,107 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Pop_up_system : MonoBehaviour
+namespace Stranding
 {
-    [SerializeField]
-    private Data_storage storage;
-    [SerializeField]
-    private GameObject popupNotification;
-    [SerializeField]
-    private GameObject choicesTab;
-
-    private void Start()
+    class Pop_up_system : MonoBehaviour
     {
-        popupNotification.SetActive(false);
-        choicesTab.SetActive(false);
-    }
+        [SerializeField]
+        private Data_storage storage;
+        [SerializeField]
+        private GameObject notification;
+        [SerializeField]
+        private Text notificationText;
+        [SerializeField]
+        private GameObject choicesTab;
 
-    private void Update()
-    {
-        
-    }
+        private BlockEvent _currentEvent;
+        public BlockEvent CurrentEvent
+        {
+            get
+            {
+                return _currentEvent;
+            }
+            set
+            {
+                if (value is NotificationEvent)
+                {
+                    _currentEvent = value;
+                    StartSendingNotification((value as NotificationEvent).message);
+                }
+                else if (value is ChoiceEvent)
+                {
 
-    private void OnClosingNotification()
-    {
+                }
+                else
+                {
+                    // Assignment failed
+                    _currentEvent = null;
+                }
+            }
+        }
 
+        private string message;
+        private bool isSendingText = false;
+        private int currentTextCounter = 0;
+        private float textSendingSpeed = 14f;
+        private float currentTextPosition = 0;
+
+        private void Start()
+        {
+            notification.SetActive(false);
+            choicesTab.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (notification.activeInHierarchy && isSendingText)
+            {
+                currentTextPosition += textSendingSpeed * Time.deltaTime;
+                currentTextCounter = (int)currentTextPosition;
+                if (currentTextCounter > message.Length)
+                {
+                    isSendingText = false;
+                }
+                else
+                {
+                    notificationText.text = message.Substring(0, currentTextCounter);
+                }
+            }
+        }
+
+        public void StartSendingNotification(string message)
+        {
+            if (message == "")
+            {
+                Debug.LogError("Message cannot be empty");
+            }
+            else
+            {
+                notification.SetActive(true);
+                this.message = message;
+                isSendingText = true;
+                currentTextPosition = 0f;
+            }
+        }
+
+        public void OnClosingNotification()
+        {
+            notification.SetActive(false);
+            if (CurrentEvent != null)
+            {
+                if (CurrentEvent is NotificationEvent)
+                {
+                    CurrentEvent.isComplete = true;
+                }
+            }
+        }
+
+        public void OnChoosingOption()
+        {
+
+        }
     }
 }
+
