bb5ca46ea0997dbfb2fa13b0ca77781a56782b85
Xiao <chairmantoad@gmail.com>
2020-10-04 20:18:32 +0800

    Continue working on loader

--- Stranding/Assets/Scripts/Loader.cs
+++ Stranding/Assets/Scripts/Loader.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
 namespace Stranding
 {
+    /// <summary>
+    /// Loads Events into the blocks according to a text file input
+    /// </summary>
     public class Loader : MonoBehaviour
     {
         private readonly string filename = "Text/content.txt";
@@ -15,8 +19,8 @@ namespace Stranding
             TextAsset contentFile = Resources.Load<TextAsset>(filename);
 
             // Parse out each line
-            StringReader reader = new StringReader(contentFile.text);
-
+            List<string> lines = new List<string>(contentFile.text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries));
+            // Remove comment lines
         }
     }
 }
