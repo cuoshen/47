faa74a2499c6d4ade7efa2f23911650725af2eb2
Xiao <chairmantoad@gmail.com>
2020-10-04 14:56:55 +0800

    Start working on loader

--- Stranding/Assets/Scripts/Loader.cs
+++ Stranding/Assets/Scripts/Loader.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 namespace Stranding
 {
     public class Loader : MonoBehaviour
     {
+        private readonly string filename = "Text/content.txt";
 
+        private void Start()
+        {
+            // Read text file
+            TextAsset contentFile = Resources.Load<TextAsset>(filename);
+
+            // Parse out each line
+            StringReader reader = new StringReader(contentFile.text);
+
+        }
     }
 }
