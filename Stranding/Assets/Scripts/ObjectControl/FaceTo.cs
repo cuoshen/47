1feb0c7574bdcb7f4470d07fe5c5811bbeeb52ac
linzexin <linzexinmasterchief@gmail.com>
2020-10-05 18:07:57 +0800

    adjusted environment effect

---
+++ Stranding/Assets/Scripts/ObjectControl/FaceTo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Stranding.Testing
+{
+    /// <summary>
+    /// always face to target
+    /// </summary>
+    public class FaceTo : MonoBehaviour
+    {
+
+        public Transform target;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.LookAt(target);
+        }
+    }
+}
