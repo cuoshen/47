d6f733dbb4124514ce318e46c699e9f62d406c31
Xiao <chairmantoad@gmail.com>
2020-10-05 14:24:08 +0800

    Title scene update

---
+++ Stranding/Assets/Scripts/FullScreenBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Stranding
+{
+    public class FullScreenBackground : MonoBehaviour
+    {
+        void Awake()
+        {
+            SpriteRenderer sr = GetComponent<SpriteRenderer>();
+            if (sr == null) return;
+
+            transform.localScale = new Vector3(1, 1, 1);
+
+            float width = sr.sprite.bounds.size.x;
+            float height = sr.sprite.bounds.size.y;
+
+            transform.localScale = new Vector2(Screen.width / width, Screen.height / height);
+        }
+    }
+}
+
