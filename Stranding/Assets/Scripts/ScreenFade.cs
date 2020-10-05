a3af9636f031031af199057a5ae475ccff6bc0e6
Xiao <chairmantoad@gmail.com>
2020-10-05 15:32:28 +0800

    title scene done

---
+++ Stranding/Assets/Scripts/ScreenFade.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Stranding
+{
+    class ScreenFade : MonoBehaviour
+    {
+        public bool isFading;
+        public bool hasFaded { get; private set; }
+
+        private float progress = 0f;
+        private float speed = 0.5f;
+        private SpriteRenderer image;
+
+        private void Start()
+        {
+            image = GetComponent<SpriteRenderer>();
+            isFading = false;
+            hasFaded = false;
+            Color transparent = image.color;
+            transparent.a = 0;
+            image.color = transparent;
+        }
+
+        private void Update()
+        {
+            if (isFading)
+            {
+                if (progress <= 1f)
+                {
+                    progress += Time.deltaTime * speed;
+                    image.color = new Color(image.color.r, image.color.g, image.color.b, progress);
+                }
+                else
+                {
+                    hasFaded = true;
+                }
+            }
+        }
+    }
+}
--- Stranding/Assets/Scripts/TitleSceneControl.cs
+++ Stranding/Assets/Scripts/TitleSceneControl.cs
@@ -1,18 +1,73 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class TitleSceneControl : MonoBehaviour
+namespace Stranding
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// The title scene also handles the very first part (pure visual novel)
+    /// </summary>
+    public class TitleSceneControl : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        private GameObject titleScreen;
+        [SerializeField]
+        private GameObject vnSuite;
+        [SerializeField]
+        private Text textBox;
+        [SerializeField]
+        private ScreenFade fade;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public Queue<string> Lines;
+        private bool isAtTitleScreen;
+        private readonly string filename = "Text/firstPart";
+
+        private void Start()
+        {
+            titleScreen.SetActive(true);
+            vnSuite.SetActive(false);
+            isAtTitleScreen = true;
+
+            // Parse out contents
+            TextAsset contentFile = Resources.Load<TextAsset>(filename);
+            if (contentFile == null)
+            {
+                Debug.LogError("Content file not found");
+            }
+            List<string> lines = new List<string>(contentFile.text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries));
+            Lines = new Queue<string>(lines);
+        }
+
+        private void Update()
+        {
+            if (!isAtTitleScreen && Input.anyKeyDown)
+            {
+                if (Lines.Count > 0)
+                {
+                    textBox.text = Lines.Dequeue();
+                }
+                else
+                {
+                    // Screen fade away
+                    fade.isFading = true;
+                }
+            }
+
+            if (Input.anyKeyDown && isAtTitleScreen)
+            {
+                titleScreen.SetActive(false);
+                vnSuite.SetActive(true);
+                isAtTitleScreen = false;
+            }
+
+            if (fade.hasFaded)
+            {
+                // Change scene
+                SceneManager.LoadScene(1);
+            }
+        }
     }
 }
+
